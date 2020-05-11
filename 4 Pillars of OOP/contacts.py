b32e5838c2af7ffdc5ca6343a82429c17060d975
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-12 00:19:18 +0530

    Extending built-ins

--- 4 Pillars of OOP/contacts.py
+++ 4 Pillars of OOP/contacts.py
@@ -1,19 +1,42 @@
 #%%
+
+class ContactList(list):
+    def search(self,name):
+        """Method to search name from contacts"""
+        matching_contact=[]
+        for contact in self:
+            if name in contact.name:
+                matching_contact.append(contact)
+        return matching_contact
 class Contact:
-    all_contacts=[] #class variables.
+    all_contacts = ContactList() #class variables.
     def __init__(self, name, email):
         self.email = email
         self.name = name
         Contact.all_contacts.append(self) #add functionality to an existing class
+    
 
 class Supplier(Contact):
     def order(self,order):
         print(f'Order will be sent to {self.name} and order is {order}')
-c = Contact("Somebody","somebody@gmail.com")
-s = Supplier("Supplier","sup@gmail.com")
-print(c.name,c.email,s.name,s.email)
-print(c.all_contacts)
-# print(c.order("I need pliers"))
-print(s.order("I need pliers"))
+
+c1 = Contact("John A", "johna@example.net")
+c2 = Contact("John B", "johnb@example.net")
+c3 = Contact("Jenna C", "jennac@example.net")
+print([c.name for c in Contact.all_contacts.search('John')])
+# %%class LongNameDict(dict):
+class LongNameDict(dict):
+    def longest_key(self):
+        longest = None
+        for key in self:
+            if not longest or len(key) > len(longest):
+                longest = key
+        return longest
+longkeys = LongNameDict()
+longkeys['hello'] = 1
+longkeys['longest yet'] = 5
+longkeys['hello2'] = 'world'
+longkeys.longest_key()
+
 
 # %%
