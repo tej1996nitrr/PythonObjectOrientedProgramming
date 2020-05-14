86b4cecfedf54afb9fadbb6bf57aea94773483ce
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-14 19:04:59 +0530

    apartment purchase

--- 4 Pillars of OOP/casestudy/real_estate.py
+++ 4 Pillars of OOP/casestudy/real_estate.py
@@ -124,3 +124,10 @@ class ApartmentRental(Rental, Apartment):
         init = Apartment.prompt_init()
         init.update(Rental.prompt_init())
         return init
+
+class ApartmentPurchase(Purchase, Apartment):
+    @staticmethod
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
