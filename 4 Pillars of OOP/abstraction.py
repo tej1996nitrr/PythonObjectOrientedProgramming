a8a06f0242df250382dcdca1ff66aaf53b4ef33f
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-18 12:13:22 +0530

    polymorphism

--- 4 Pillars of OOP/abstraction.py
+++ 4 Pillars of OOP/abstraction.py
@@ -32,4 +32,3 @@ class Ogg(MediaLoader):
         pass
 o = Ogg()
 
-# %%
--- 4 Pillars of OOP/polymorphism.py
+++ 4 Pillars of OOP/polymorphism.py
@@ -1,4 +1,5 @@
 #%%
+"""Ad-hoc polymorphism through function overloading is not possible in Python. """
 class AudioFile:
     def __init__(self, filename):
         """The __init__ method in the parent class is able to access
@@ -39,3 +40,60 @@ class FlacFile:
         self.filename = filename
     def play(self):
         print("playing {} as flac".format(self.filename))
+#%%
+# Duck Typing
+class Shark():
+    def swim(self):
+        print('Shark is Swimming')
+    def swim_backwards(self):
+        print("shark can't swim backwards")
+    def skeleton(self):
+        print("sharks skeleton is made of cartilage"
+        )
+class Clownfish():
+    def swim(self):
+        print("The clownfish is swimming.")
+
+    def swim_backwards(self):
+        print("The clownfish can swim backwards.")
+
+    def skeleton(self):
+        print("The clownfish's skeleton is made of bone.")
+sammy = Shark()
+sammy.skeleton()
+
+casey = Clownfish()
+casey.skeleton()
+
+# %%
+#Method Overriding -dynamic polymorphism
+class Bird: 
+    def intro(self): 
+        print("There are many types of birds.") 
+        
+    def flight(self): 
+	    print("Most of the birds can fly but some cannot.") 
+	
+class sparrow(Bird): 
+    def flight(self): 
+	    print("Sparrows can fly.") 
+	
+class ostrich(Bird): 
+    def flight(self): 
+        print("Ostriches cannot fly.") 
+	
+obj_bird = Bird() 
+obj_spr = sparrow() 
+obj_ost = ostrich() 
+
+obj_bird.intro() 
+obj_bird.flight() 
+
+obj_spr.intro() 
+obj_spr.flight() 
+
+obj_ost.intro() 
+obj_ost.flight() 
+
+
+# %%
